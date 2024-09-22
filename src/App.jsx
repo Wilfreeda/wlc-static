20d10e67662f892babae6176dea5fdfd44a312d1
Wilfreeda's Language Centre - WLC. <wilfreedaslanguagecentre@gmail.com>
2024-09-22 18:19:28 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Contact from "./components/contact/Contact";
 import Cursor from "./components/cursor/Cursor";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
-import Service from "./components/services/service";
+import Service from "./components/services/Service";
 
 const App = () => {
   return (
